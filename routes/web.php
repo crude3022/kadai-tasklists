0405ff5ed69e826d59bf14fc8fc2bc81f5eb2609
hiroyuki.sumiuchi <crude0708@gmail.com>
2021-03-23 07:07:56 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', 'TasklistsController@index');
 
 Route::resource('tasklist', 'TasklistsController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
